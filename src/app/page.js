1d30f6590751dbbc6de83400b0df7ba97b7001a6
vieiraBeh <brendanascimentovieirah@gmail.com>
2024-03-23 09:16:40 -0400

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,20 @@
-const url = "http://localhost:3000/api";
+const url = "https://back-end-ifms-tau.vercel.app/campi";
 
 export default async function Home() {
 
   const resposta = await fetch(url,{
-    cache:"no-cache"
+   next:{
+    revalidate: 1
+   }
   });
-  const campus = await resposta.json();
+  const campi = await resposta.json();
  
   return (
     <main>
       <h1>Home</h1>
-      {campus.map((campi) =>
+      {campi.map((campus) =>
         <div>
-             <p>{campi.nome_campi}</p>
+             <p>{campus.nome_campus}</p>
         </div>
       )}
     </main>
